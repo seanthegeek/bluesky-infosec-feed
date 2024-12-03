35248e404bd9f07f7e5485cb7735eaba17e581f0
Sean Whalen <seanthegeek@users.noreply.github.com>
2024-12-02 22:38:53 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Installing git..."
+sudo apt install -y git
+echo "Installing python3-virtualenv..."
+sudo apt install -y python3-virtualenv
+echo "Installing redis..."
+sudo apt install -y redis
+sudo sysctl enable redis-server
+sudo sysctl start redis-server
+if id blueskyunfosecfeed >/dev/null 2>&1; then
+    echo "blueskyinfosecfeed user already exists."
+else
+    echo "creating blueskyinfosecfeed user..."
+    sudo useradd -sm blueskyinfosecfeed -b /opt
+fi
+sudo cd ~blueskyinfosecfeed
+if [ ! -d "bluesky-infosec-feed" ]; then
+  sudo -u blueskyinfosecfeed git clone https://github.com/seanthegeek/bluesky-infosec-feed
+fi
+sudo cd bluesky-infosec-feed
+sudo -u blueskyinfosecfeed git pull
+sudo cp systemd/* /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable blueskyinfosecfeed.service
+sudo systemctl start blueskyinfosecfeed.service
+sudo systemctl enable update-blueskyinfosecfeed.timer
+sudo systemctl start update-blueskyinfosecfeed.timer
