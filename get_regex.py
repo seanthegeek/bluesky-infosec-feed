b12602255da2990b08b93dc983dd063369548841
Sean Whalen <seanthegeek@users.noreply.github.com>
2024-12-06 18:47:43 -0500

    Update get_regex.py

--- get_regex.py
+++ get_regex.py
@@ -2,9 +2,7 @@ import redis
 
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 keys = ["infosec_keywords_regex", "infosec_keywords_case_sensitive_regex"]
-output = r""
 for key in keys:
     value = r.get(key)
     if value:
-        output += value
-print(output)
+        print(f"{key}\n{value}\n\n")
