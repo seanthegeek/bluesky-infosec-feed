d997655a96c5996bb11498957ef7a876389baa4c
Sean Whalen <seanthegeek@users.noreply.github.com>
2024-12-07 09:37:09 -0500

    Be more graceful about firehose errors in production
    Log the exception instead of raising it unless the logging level is set to `DEBUG` (such as when the Flask development server is running)

--- server/data_stream.py
+++ server/data_stream.py
@@ -1,3 +1,4 @@
+import logging
 from collections import defaultdict
 
 from atproto import AtUri, CAR, firehose_models, FirehoseSubscribeReposClient, models, parse_subscribe_repos_message
@@ -52,7 +53,9 @@ def run(name, operations_callback, stream_stop_event=None):
             _run(name, operations_callback, stream_stop_event)
         except FirehoseError as e:
             # here we can handle different errors to reconnect to firehose
-            logger.error(f"{e} - Reconnecting to the firehose...")
+            if logger.level == logging.DEBUG:
+                raise e
+            logger.error(f"Firehose error: {e}. Reconnecting to the firehose.")
 
 
 def _run(name, operations_callback, stream_stop_event=None):
