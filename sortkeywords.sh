082d13ec8a56acf2561a08ab37032660c331e237
Sean Whalen <seanthegeek@users.noreply.github.com>
2024-12-09 14:24:56 -0500

    Add more vendors

--- sortkeywords.sh
+++ sortkeywords.sh
@@ -3,6 +3,3 @@
 sort -o lists/keywords.txt lists/keywords.txt
 sort -o lists/keywords_case_sensitive.txt lists/keywords_case_sensitive.txt
 sort -o lists/keywords_vendors.txt lists/keywords_vendors.txt
-csvsort lists/users.csv
-csvsort lists/ignore_users.csv
-csvsort lists/ignore_keywords.csv
